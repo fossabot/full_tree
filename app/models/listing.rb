a8d314ff6dd57795e5ec8ca1e1b98e56782604e5
Nika Miller <nmiller@shareableink.com>
2014-10-22 12:56:12 -0500

    added title to listings, changed views/model, cleaned up listings page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,13 @@
 class Listing < ActiveRecord::Base
-  has_one :listing_type
-  has_one :listing_category
+  belongs_to :listing_type
+  belongs_to :listing_category
+  belongs_to :user
+  belongs_to :community
+  belongs_to :share_type
+  belongs_to :share_category
+
+  def pretty_title
+    title.split(' ').map { |x| x.capitalize }.join(' ')
+  end
 end
+
